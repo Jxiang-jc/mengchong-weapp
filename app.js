9dfc3225653ee84356fb2d0cee2c7d7db77b2c17
Jxiang-jc <zjx1020645@163.com>
2018-11-24 14:52:28 +0800

    基础已经完善

--- app.js
+++ app.js
@@ -5,10 +5,22 @@ App({
   },
 
   onLaunch (options) {
+    
     try {
-      const res = wx.getSystemInfoSync()
-      this.globalData.windowHeight = res.windowHeight;
-      this.globalData.windowWidth = res.windowWidth;
+      const deviceInfo = wx.getStorageSync('deviceInfo')
+      if (!deviceInfo) {
+        const res = wx.getSystemInfoSync()
+        this.globalData.windowHeight = res.windowHeight;
+        this.globalData.windowWidth = res.windowWidth;
+        wx.setStorageSync('deviceInfo',{
+          windowHeight: res.windowHeight,
+          windowWidth: res.windowWidth
+        })
+      } else {
+        this.globalData.windowHeight = deviceInfo.windowHeight;
+        this.globalData.windowWidth = deviceInfo.windowWidth;
+      }
+      
     } catch (e) {
       // Do something when catch error
     }
